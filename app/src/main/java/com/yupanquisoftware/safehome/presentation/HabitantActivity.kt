796ac7060fee66546d81bf48931c63bc896c6965
Cornejo <cristian.cornejo@unmsm.edu.pe>
2019-11-12 09:48:11 -0500

    aun ando yoni

---
+++ app/src/main/java/com/yupanquisoftware/safehome/presentation/HabitantActivity.kt
@@ -0,0 +1,13 @@
+package com.yupanquisoftware.safehome.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yupanquisoftware.safehome.R
+
+class HabitantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_habitant)
+    }
+}
